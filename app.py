e2974de545be37a6b05c9f8880b9dae23c39e61f
Junyan Lu <junyanlu@Junyans-MacBook-Pro.local>
2024-03-26 16:33:38 -0700

    Your commit message

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from annotated_text import annotated_text
 
-st.title('My First Streamlit App')
-st.write('Hello, Streamlit!')
+st.title("👋Hola! I'm Junyan")
+st.image('./images/top.png')
+st.header('🙋🏻‍♂️About Me')
+st.markdown("""
+- Who I am
+- Where I live
+""")
