48abd8d60ae045d523c440b7e3df1b7b256e86ac
Junyan Lu <junyanlu@Junyans-MacBook-Pro.local>
2024-04-01 23:24:02 -0700

    updated

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import streamlit as st
 st.title("👋Hola! I'm Junyan")
 st.image('./images/top.png')
 st.header('🙋🏻‍♂️About Me')
-st.markdown("""
-- Who I am
-- Where I live
+st.markdown(
+"""
+Heya! I'm Junyan Lu
+I am motivated to become a product designer specialized in UX/UI. Currently live in
+Long Island City, NY.
+My career goal is to use technological innovation to help people enjoy a more convenient life and assist elders with their daily life.
+Undefined, I like to attempt different styles to express my thoughts.
 """)
